9bfa5919379433389823a7ac0ab8b24576673e01
SYU <xeungjin@gmail.com>
2025-02-27 12:49:43 +0900

    ~ 584 page

--- board/src/main/java/board/jdbc/DBCPInitListener.java
+++ board/src/main/java/board/jdbc/DBCPInitListener.java
@@ -1,5 +1,79 @@
 package board.jdbc;
 
-public class DBCPInitListener {
+import java.io.IOException;
+import java.io.StringReader;
+import java.util.Properties;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.tomcat.dbcp.dbcp2.ConnectionFactory;
+import org.apache.tomcat.dbcp.dbcp2.DriverManagerConnectionFactory;
+import org.apache.tomcat.dbcp.dbcp2.PoolableConnectionFactory;
+import org.apache.tomcat.dbcp.pool2.impl.GenericObjectPoolConfig;
+
+public class DBCPInitListener implements ServletContextListener {
+
+	@Override
+	public void contextInitialized(ServletContextEvent sce) {
+		String poolConfig = sce.getServletContext().getInitParameter("poolConfig");
+		Properties prop = new Properties();
+		try {
+			prop.load(new StringReader(poolConfig));
+		} catch (IOException e) {
+			throw new RuntimeException("config load fail", e);
+		}
+		loadJDBCDriver(prop);
+		initConnectionPool(prop);
+	}
+
+	private void loadJDBCDriver(Properties prop) {
+		String driverClass = prop.getProperty("jdbcdriver");
+		try {
+			Class.forName(driverClass);
+		} catch (ClassNotFoundException ex) {
+			throw new RuntimeException("fail to load JDBC Driver", ex);
+		}
+	}
+
+	private void initConnectionPool(Properties prop) {
+		try {
+			String jdbcUrl = prop.getProperty("jdbcUrl");
+			String username = prop.getProperty("dbUser") ;
+			String pw = prop.getProperty("dbPass");
+			
+			ConnectionFactory connFactory = new DriverManagerConnectionFactory(jdbcUrl, username, pw);
+			PoolableConnectionFactory poolableConnFactory = new PoolableConnectionFactory(connFactory, null);
+			String validationQuery = prop.getProperty("validationQuery");
+			if(validationQuery != null && !validationQuery.isEmpty()) {
+				poolableConnFactory.setValidationQuery(validationQuery);
+			}
+			GenericObjectPoolConfig poolConfig = new GenericObjectPoolConfig();
+			poolConfig.setTimeBetweenEvictionRunsMillis(1000L * 60L *5L);
+			poolConfig.setTestWhileIdle(true);
+			int minIdle = getIntProperty(prop, "minIdle", 5);
+			poolConfig.setMinIdle(minIdle);
+			
+			
+			
+			
+			
+			
+		}catch(Exception e) {
+			
+		}
+
+	}
+
+	private int getIntProperty(Properties prop, String string, int i) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void contextDestroyed(ServletContextEvent sce) {
+		// TODO Auto-generated method stub
+		ServletContextListener.super.contextDestroyed(sce);
+	}
 
 }
